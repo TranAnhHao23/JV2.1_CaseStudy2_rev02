9e3d750339f85ce54a61c7f3467653d794036dd6
TranAnhHao23 <Trananhhaonuce@gmail.com>
2022-01-06 23:37:58 +0700

    Fourth Commit

--- src/menu_display/MenuDisplay.java
+++ src/menu_display/MenuDisplay.java
@@ -98,16 +98,17 @@ public class MenuDisplay {
                     cyberManager.changePlayPrice(priceNew);
                     break;
                 case 8:
-                    MenuRevenue menuRevenue = new MenuRevenue();
-                    menuRevenue.menuRevenue();
+                    MenuPayment menuPayment = new MenuPayment();
+                    menuPayment.menuPayment();
                     break;
                 case 9:
                     MenuAccountManager menuAccountManager = new MenuAccountManager();
                     menuAccountManager.menuAccountManager();
                     break;
                 case 10:
+                    MenuRevenue menuRevenue = new MenuRevenue();
                     revenueManager.addRevenue();
-                    totalPay();
+                    menuRevenue.totalPay();
                     break;
                 case 11:
                     System.out.println("Căm bách về menu đăng nhập nhớ");
@@ -120,47 +121,6 @@ public class MenuDisplay {
                     break;
             }
         } while (choice != 11);
-    }
-
-    public void totalPay() {
-        int menuTotalPayChoice;
-        do {
-            System.out.println("1. Tổng doanh thu từ đầu");
-            System.out.println("2. Chốt doanh thu ngày hôm nay");
-            System.out.println("3. Tổng doanh thu theo ngày");
-            System.out.println("0.Quay lại");
-            System.out.println("------------------");
-            System.out.print("Chọn đi nào: ");
-            menuTotalPayChoice = scanner.nextInt();
-            scanner.nextLine();
-            switch (menuTotalPayChoice) {
-                case 1:
-                    System.out.println(revenueManager.totalRevenue());
-                    ;
-                    break;
-                case 2:
-                    System.out.println(revenueManager.revenueToday());
-                    ;
-                    break;
-                case 3:
-                    System.out.print("Từ ngày (dd/MM/yyyy): ");
-                    String dateStart = scanner.nextLine();
-                    System.out.print("Đến ngày (dd/MM/yyyy): ");
-                    String dateEnd = scanner.nextLine();
-                    double revenuePeriod = revenueManager.revenuePeriod(dateStart, dateEnd);
-                    if (revenuePeriod == -1 || dateStart.compareTo(dateEnd) >= 0) {
-                        System.out.println("Có gì đó sai sai?");
-                    } else {
-                        System.out.println("Doanh thu từ " + dateStart + " đến " + dateEnd + " là: " + revenuePeriod);
-                    }
-                case 0:
-                    System.out.println("Bái bai");
-                    break;
-                default:
-                    System.out.println("Không có lựa chọn đó cho bạn đâu -_-");
-                    break;
-            }
-        } while (menuTotalPayChoice != 0);
     }
 
     public String onlineComputer() {
--- src/menu_display/MenuRevenue.java
+++ src/menu_display/MenuRevenue.java
@@ -1,52 +1,45 @@
 package menu_display;
 
-import model.Service;
-
 import java.util.Scanner;
 
-import static menu_display.MenuDisplay.cyberManager;
-import static menu_display.MenuDisplay.serviceManager;
+import static menu_display.MenuDisplay.revenueManager;
 
 public class MenuRevenue {
-    Scanner scanner = new Scanner(System.in);
-    MenuDisplay menuDisplay = new MenuDisplay();
+    private Scanner scanner = new Scanner(System.in);
 
     public MenuRevenue() {
     }
-
-    public void menuRevenue() {
-        int menuRevenueChoice;
+    public void totalPay() {
+        int menuTotalPayChoice;
         do {
-            System.out.println("1. Thêm dịch vụ");
-            System.out.println("2. Thanh toán");
-            System.out.println("0. Quay về");
-            System.out.println("---------------");
+            System.out.println("1. Tổng doanh thu từ đầu");
+            System.out.println("2. Chốt doanh thu ngày hôm nay");
+            System.out.println("3. Tổng doanh thu theo ngày");
+            System.out.println("0.Quay lại");
+            System.out.println("------------------");
             System.out.print("Chọn đi nào: ");
-            menuRevenueChoice = scanner.nextInt();
+            menuTotalPayChoice = scanner.nextInt();
             scanner.nextLine();
-            switch (menuRevenueChoice) {
+            switch (menuTotalPayChoice) {
                 case 1:
-                    System.out.print("Nhập ID máy muốn thêm dịch vụ: ");
-                    int idAddServices = scanner.nextInt();
-                    scanner.nextLine();
-                    if (menuDisplay.onlineComputer().contains(String.valueOf(idAddServices))) {
-//                        selectService();
-                        cyberManager.addServiceToComputer(idAddServices, selectService());
-                    } else {
-                        System.out.println("Máy đang offline bạn ôi");
-                    }
+                    System.out.println(revenueManager.totalRevenue());
+                    ;
                     break;
                 case 2:
-                    System.out.print("Nhập ID muốn thanh toán: ");
-                    int idPayment = scanner.nextInt();
-                    scanner.nextLine();
-                    if (menuDisplay.onlineComputer().contains(String.valueOf(idPayment))) {
-                        System.out.println("Tắt máy tính tiền");
-                        System.out.println("Tổng tiền của máy" + idPayment + " = " + cyberManager.turnOffComputer(idPayment));
-                    }else {
-                        System.out.println("Máy đang offline bạn ôi");
-                    }
+                    System.out.println(revenueManager.revenueToday());
+                    ;
                     break;
+                case 3:
+                    System.out.print("Từ ngày (dd/MM/yyyy): ");
+                    String dateStart = scanner.nextLine();
+                    System.out.print("Đến ngày (dd/MM/yyyy): ");
+                    String dateEnd = scanner.nextLine();
+                    double revenuePeriod = revenueManager.revenuePeriod(dateStart, dateEnd);
+                    if (revenuePeriod == -1 || dateStart.compareTo(dateEnd) >= 0) {
+                        System.out.println("Có gì đó sai sai?");
+                    } else {
+                        System.out.println("Doanh thu từ " + dateStart + " đến " + dateEnd + " là: " + revenuePeriod);
+                    }
                 case 0:
                     System.out.println("Bái bai");
                     break;
@@ -54,32 +47,6 @@ public class MenuRevenue {
                     System.out.println("Không có lựa chọn đó cho bạn đâu -_-");
                     break;
             }
-        } while (menuRevenueChoice != 0);
+        } while (menuTotalPayChoice != 0);
     }
-
-    private Service selectService() {
-        Service serviceAdd = null;
-        for (Service service : serviceManager.displayService()) {
-            System.out.println(service);
-        }
-        System.out.print("Bạn muốn chọn dịch vụ nào: ");
-        int idService = scanner.nextInt();
-        System.out.print("Số lượng bao nhiêu nhở? ");
-        int quantity = scanner.nextInt();
-        for (Service service : serviceManager.displayService()) {
-            if (service.getId() == idService) {
-                serviceAdd = service;
-                serviceAdd.setQuantity(quantity);
-                System.out.println("Thêm thành công");
-                break;
-            }
-        }
-        if (serviceAdd == null) {
-            System.out.println("Bạn nhập sai rồi đó");
-        }
-        System.out.println("Bạn muốn thêm gì nữa không. Nhấn 1 để thêm, nhấn 0 để thoát");
-        return serviceAdd;
-    }
-
-
 }
